dd03849fa0a40bfff93975e7553075fcf20bb1d5
Mary Robin Rose <maryrobinrose@gmail.com>
2019-03-12 17:16:02 -0700

    remove old comments

--- js/script.js
+++ js/script.js
@@ -3,40 +3,9 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
 
 //Selects all students from main page
 const studentList = document.querySelectorAll('.student-item');
-
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
 
 //Hide students from list, show select students at a time
 const showPage = (list, page) => {
@@ -50,12 +19,6 @@ const showPage = (list, page) => {
     }
   }
 };
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
 
 //Function creates div for list of students and appends page numbers and buttons
 const appendPageLinks = (list) => {
@@ -84,14 +47,14 @@ const appendPageLinks = (list) => {
     li.appendChild(a);
     ul.appendChild(li);
 
-      //Loop over pagination links to remove active class from all links
-      for (let i = 0; i <= a.length; i += 1) {
-        a[i].addEventListener('click', () => {
-        let aTag = document.querySelectorAll('a');
-        aTag[0].className = aTag[0].className.remove('active');
-        this.className += ' active';
-        });
-      }
+    //Loop over pagination links to remove active class from all links
+    for (let i = 0; i <= a.length; i += 1) {
+      a[i].addEventListener('click', () => {
+      let aTag = document.querySelectorAll('a');
+      aTag[0].className = aTag[0].className.remove('active');
+      this.className += ' active';
+      });
+    }
   }
 }
 
